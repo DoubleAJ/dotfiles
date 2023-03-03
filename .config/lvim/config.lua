d6c23059b8d3f93733c79bf054c4f1067b24cc8b
Alex Maraldi <alex.maraldi@gmail.com>
2023-03-03 14:16:35 -0500

    Better debugging keymapping. Visualize variable values in virtual text.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -27,6 +27,16 @@ lvim.leader = "space"
 lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 lvim.keys.normal_mode["<S-l>"] = ":BufferLineCycleNext<CR>"
 lvim.keys.normal_mode["<S-h>"] = ":BufferLineCyclePrev<CR>"
+-- Debugging
+lvim.keys.normal_mode["<F5>"] = ":lua require('dap').continue()<CR>"
+lvim.keys.normal_mode["<F10>"] = ":lua require('dap').step_over()<CR>"
+lvim.keys.normal_mode["<F11>"] = ":lua require('dap').step_into()<CR>"
+lvim.keys.normal_mode["<F12>"] = ":lua require('dap').step_out()<CR>"
+lvim.keys.normal_mode["<F6>"] = ":lua require('dap.ui.widgets').hover()<CR>"
+-- The following don't work...
+-- lvim.keys.normal_mode["<F7>"] = ":lua require('dap.ui.variables').hover()<CR>"
+-- lvim.keys.normal_mode["<F8>"] = ":lua require('dap.ui.variables').visual_hover()<CR>"
+
 -- unmap a default keymapping
 -- vim.keymap.del("n", "<C-Up>")
 -- override a default keymapping
@@ -312,6 +322,41 @@ lvim.plugins = {
 --       "folke/trouble.nvim",
 --       cmd = "TroubleToggle",
 --     },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    requires = {
+      "mfusseneger/nvim-dap",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("nvim-dap-virtual-text").setup {
+        enabled = true,                        -- enable this plugin (the default)
+        enabled_commands = true,               -- create commands DapVirtualTextEnable, DapVirtualTextDisable, DapVirtualTextToggle, (DapVirtualTextForceRefresh for refreshing when debug adapter did not notify its termination)
+        highlight_changed_variables = true,    -- highlight changed values with NvimDapVirtualTextChanged, else always NvimDapVirtualText
+        highlight_new_as_changed = false,      -- highlight new variables in the same way as changed variables (if highlight_changed_variables)
+        show_stop_reason = true,               -- show stop reason when stopped for exceptions
+        commented = false,                     -- prefix virtual text with comment string
+        only_first_definition = true,          -- only show virtual text at first definition (if there are multiple)
+        all_references = false,                -- show virtual text on all all references of the variable (not only definitions)
+        --- A callback that determines how a variable is displayed or whether it should be omitted
+        --- @param variable Variable https://microsoft.github.io/debug-adapter-protocol/specification#Types_Variable
+        --- @param buf number
+        --- @param stackframe dap.StackFrame https://microsoft.github.io/debug-adapter-protocol/specification#Types_StackFrame
+        --- @param node userdata tree-sitter node identified as variable definition of reference (see `:h tsnode`)
+        --- @return string|nil A text how the virtual text should be displayed or nil, if this variable shouldn't be displayed
+        display_callback = function(variable, _buf, _stackframe, _node)
+          return variable.name .. ' = ' .. variable.value
+        end,
+
+        -- experimental features:
+        virt_text_pos = 'eol',                 -- position of virtual text, see `:h nvim_buf_set_extmark()`
+        all_frames = false,                    -- show virtual text for all stack frames not only current. Only works for debugpy on my machine.
+        virt_lines = false,                    -- show virtual lines instead of virtual text (will flicker!)
+        virt_text_win_col = nil                -- position the virtual text at a fixed window column (starting from the first text column) ,
+                                               -- e.g. 80 to position at column 80, see `:h nvim_buf_set_extmark()`
+      }
+    end,
+  },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
