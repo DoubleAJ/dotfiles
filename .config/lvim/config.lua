d0a3198f5fc453967cdd48f5fa09145797db2b09
Alex Maraldi <alex.maraldi@gmail.com>
2023-06-05 20:54:14 -0400

    Added go to definition/declaration keymaps for C/C++.

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -402,6 +402,15 @@ end
 local on_clangd_attach = function()
   -- Keymap to switch between source and header
   vim.api.nvim_set_keymap("n", "<Leader>lh", "<cmd>:ClangdSwitchSourceHeader<Cr>", { noremap = true, silent = true })
+  -- If the 'go to definition' command does not work, it is probably because
+  -- 'compile_commands.json' must be generated in the project root directory
+  -- (see https://clang.llvm.org/docs/JSONCompilationDatabase.html ).
+  -- The 'bear' tool can be used to generate it. It can be installed normally via the distro repos.
+  -- In order to use it, start from a clean build, then call 'bear -- make' 
+  -- (or whatever command is used to build instead of 'make').
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation)
   lldb_dap_configs_setup()
 end
 
